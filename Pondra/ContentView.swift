af5b6687c1b5b74d557dc86a9e3a7c8df4d4a3c7
Kevin Stieglitz <kevin.stieglitz@jambit.com>
2019-11-18 16:59:14 +0100

    Initial Commit

---
+++ Pondra/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pondra
+//
+//  Created by Kevin Stieglitz on 18.11.19.
+//  Copyright © 2019 Kevin Stieglitz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
